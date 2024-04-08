22e60599fc9ed061407d56a155529d6d7dac0c40
koosco <rnxogud123@naver.com>
2024-04-09 01:30:01 +0900

    User 엔티티, 리포지토리 추가

---
+++ src/main/java/io/security/springjwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package io.security.springjwt.repository;
+
+import io.security.springjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+}
